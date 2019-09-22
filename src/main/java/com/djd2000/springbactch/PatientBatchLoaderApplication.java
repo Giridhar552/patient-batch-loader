e74b56bdeec3973e12018e483ba1e418a558cb4c
David <davidd125@yahoo.com>
2019-09-22 16:32:49 +0100

    Adding initial project

---
+++ src/main/java/com/djd2000/springbactch/PatientBatchLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.djd2000.springbactch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientBatchLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientBatchLoaderApplication.class, args);
+	}
+
+}
